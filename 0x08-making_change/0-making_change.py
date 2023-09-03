fdc56d9788ae7a4ddfa27c0d55c00304d997986f
Oluwakemi Omoyeni <shininglite2012@gmail.com>
2023-09-03 20:08:01 +0100

    corrections

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -12,3 +12,14 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
+    
+    current_total = 0
+    coin_used = 0
+    coins = sorted(coins, reverse=True)
+    for coin in coins:
+        r = (total-current_total)//coin
+        current_total += r*coin
+        coin_used += r
+        if current_total == total:
+            return coin_used
+    return -1
---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""a function def island_perimeter(grid): that returns the
+perimeter of the island described in grid"""
+
+
+def island_perimeter(grid):
+    """
+    Takes an argument grid and returns the parameter
+    """
+    island_pm = 0
+    if isinstance(grid, list) == False:
+        return 0
+    rows = len(grid)
+    cols = len(grid[0])
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                island_pm += 4
+                if i > 0 and grid[i-1][j] == 1:
+                    island_pm -= 2
+                if j > 0 and grid[i][j-1] == 1:
+                    island_pm -= 2
+    return island_pm
