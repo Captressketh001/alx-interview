30e438e5d7eb93a70bce2e78d07f13a68eb52a4e
Oluwakemi Omoyeni <shininglite2012@gmail.com>
2023-09-03 20:54:43 +0100

    corrections

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """a function def island_perimeter(grid): that returns the
-perimeter of the island described in grid"""
+perimeter of the island described in grid
+"""
 
 
 def island_perimeter(grid):
